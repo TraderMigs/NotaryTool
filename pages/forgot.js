3a155f19478c3d4945e08979e1a426651f739f6c
TraderMigs <wvdreamsofimd@gmail.com>
2026-03-19 15:59:26 +0700

    Delete pages/forgot.js

--- pages/forgot.js
+++
@@ -1,9 +0,0 @@
-export default function Forgot() {
-  return (
-    <div style={{padding:40}}>
-      <h2>Forgot Password</h2>
-      <input placeholder="Email" /><br/><br/>
-      <button>Send Reset Link</button>
-    </div>
-  );
-}
