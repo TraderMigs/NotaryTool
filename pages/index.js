1db23c3770f8cfc3f25408734ae3fd0d6c819eef
TraderMigs <wvdreamsofimd@gmail.com>
2026-03-19 15:59:47 +0700

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{padding:40,fontFamily:'Arial'}}>
-      <h1>NotaryTool</h1>
-      <p>Privacy Pre-Processor for Pennsylvania Notaries</p>
-      <a href="/signup">Get Started</a>
-    </div>
-  );
-}
