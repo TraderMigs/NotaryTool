85b307e135f3f37b4b942939e3351f741624dd37
TraderMigs <wvdreamsofimd@gmail.com>
2026-03-19 13:15:06 +0700

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{padding:40,fontFamily:'Arial'}}>
+      <h1>NotaryTool</h1>
+      <p>Privacy Pre-Processor for Pennsylvania Notaries</p>
+      <a href="/signup">Get Started</a>
+    </div>
+  );
+}
