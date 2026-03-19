89cd72f8edc90934ece82c06b900d4da0606f2e0
TraderMigs <wvdreamsofimd@gmail.com>
2026-03-19 15:59:11 +0700

    Delete pages/terms.js

--- pages/terms.js
+++
@@ -1,8 +0,0 @@
-export default function Terms() {
-  return (
-    <div style={{padding:40}}>
-      <h2>Terms of Service</h2>
-      <p>This is a utility tool. No guarantees. User responsible for usage.</p>
-    </div>
-  );
-}
