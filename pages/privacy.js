4fa2d4f957e4cc00445a03a48914863abfa0c661
TraderMigs <wvdreamsofimd@gmail.com>
2026-03-19 15:59:18 +0700

    Delete pages/privacy.js

--- pages/privacy.js
+++
@@ -1,8 +0,0 @@
-export default function Privacy() {
-  return (
-    <div style={{padding:40}}>
-      <h2>Privacy Policy</h2>
-      <p>We do not store documents. Processing happens client-side.</p>
-    </div>
-  );
-}
